2197208ed0773bfaa2a2a366720bb439d76e5914
@alt_bier <richard@gowen.net>
2019-03-24 23:05:19 -0500

    Candy is dandy but liquor is quicker!

--- code/3kcode/main.py
+++ code/3kcode/main.py
@@ -21,8 +21,12 @@ led.direction = Direction.OUTPUT
 touch = touchio.TouchIn(board.D3)
 
 # NeoPixel strip connected on D4
-NUMPIXELS = 4
-neopixels = neopixel.NeoPixel(board.D4, NUMPIXELS, brightness=0.2, auto_write=False)
+NUMPIXELSD4 = 3
+neopixelsd4 = neopixel.NeoPixel(board.D4, NUMPIXELSD4, brightness=0.2, auto_write=False)
+
+# NeoPixel strip connected on D2
+NUMPIXELSD2 = 3
+neopixelsd2 = neopixel.NeoPixel(board.D2, NUMPIXELSD2, brightness=0.2, auto_write=False)
 
 # Used if we do HID output, see below
 kbd = Keyboard()
@@ -45,9 +49,11 @@ def wheeld(pos):
         return (int(pos*3), 0, int(255 - pos*3))
 
 # Helper to give us a nice color swirl on NeoPixel (G,R,B)
-def wheeln(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
+def wheeln(pos, sft):
+    if (pos + sft) > 255:
+        pos = (pos + sft) - 256
+    else:
+        pos = (pos + sft)
     if (pos < 0) or (pos > 255):
         return (0, 0, 0)
     if pos < 85:
@@ -64,6 +70,7 @@ def wheeln(pos):
 
 # Helper to simulate a red siren on NeoPixel (G,R,B)
 def sirenred(pos, sft):
+    # pos = position, sft = shift
     if (pos + sft) > 255:
         pos = (pos + sft) - 256
     else:
@@ -80,6 +87,7 @@ def sirenred(pos, sft):
         return (0, 0, 0)
 # Helper to simulate a blue siren on NeoPixel (G,R,B)
 def sirenblue(pos, sft):
+    # pos = position, sft = shift
     if (pos + sft) > 255:
         pos = (pos + sft) - 256
     else:
@@ -96,6 +104,7 @@ def sirenblue(pos, sft):
         return (0, 0, 0)
 
 def flashrw(pos, sft):
+    # pos = position, sft = shift
     posred = [1,2,3,4,9,10,11,12,17,18,19,20,25,26,27,28,129,130,131,132,137,138,139,140,145,146,147,148,153,154,155,156]
     posblue = [262,263]
     poswhite = [42,43,49,50,106,107,113,114,170,171,177,178,234,235,241,242]
@@ -116,6 +125,7 @@ def flashrw(pos, sft):
         return (0, 0, 0)
 
 def flashby(pos, sft):
+    # pos = position, sft = shift
     posred = [262,263]
     posblue = [65,66,67,68,73,74,75,76,81,82,83,84,89,90,91,92,193,194,195,196,201,202,203,204,209,210,211,212,217,218,219,220]
     poswhite = [264,265]
@@ -135,25 +145,65 @@ def flashby(pos, sft):
     else:
         return (0, 0, 0)
 
+def flashrb(pos, sft, nd):
+    # pos = position, sft = shift, nd = 0 neopixel 1 dot (flips red and green)
+    posred = [1,2,3,4,9,10,11,12,17,18,19,20,25,26,27,28,129,130,131,132,137,138,139,140,145,146,147,148,153,154,155,156]
+    posblue = [65,66,67,68,73,74,75,76,81,82,83,84,89,90,91,92,193,194,195,196,201,202,203,204,209,210,211,212,217,218,219,220]
+    poswhite = [262,263]
+    posyellow = [264,265]
+    if (pos + sft) > 255:
+        pos = (pos + sft) - 256
+    else:
+        pos = (pos + sft)
+    if pos in posred:
+        if nd == 1:
+            return (255, 0, 0)
+        else:
+            return (0, 255, 0)
+    elif pos in posblue:
+        return (0, 0, 255)
+    elif pos in poswhite:
+        return (255, 255, 255)
+    elif pos in posyellow:
+        return (255, 255, 0)
+    else:
+        return (0, 0, 0)
+
 ######################### MAIN LOOP ##############################
 i = 0
 while True:
-  # spin internal LED around! autoshow is on
+# spin internal LED around! autoshow is on
 #  dot[0] = wheeld(i & 255)
-  dot[0] = (0,0,0)
+# Turn off dot
+#  dot[0] = (0,0,0)
 
-# neopixel values = (G,R,B)
-#  neopixels[0] = sirenred(i, 0)
-#  neopixels[1] = sirenred(i, 16)
-#  neopixels[2] = sirenblue(i, 32)
-#  neopixels[3] = sirenblue(i, 8)
-  neopixels[0] = sirenred(i, 0)
-  neopixels[1] = sirenblue(i, 32)
-  neopixels[2] = flashrw(i, 0)
-  neopixels[3] = flashby(i, 0)
-  neopixels.show()
+# NeoPixels are on D2 and D4
+# NeoPixel values are in the color order (G, R, B)
 
-  # use D3 as capacitive touch to turn on internal LED
+# Bad boys, bad boys, whatchya gonna do?
+#  neopixelsd2[0] = sirenred(i, 0)
+#  neopixelsd2[1] = flashrw(i, 0)
+#  neopixelsd2[2] = flashby(i, 128)
+#  neopixelsd2.show()
+#  neopixelsd4[0] = sirenblue(i, 64)
+#  neopixelsd4[1] = flashby(i, 0)
+#  neopixelsd4[2] = flashrw(i, 128)
+#  neopixelsd4.show()
+#  dot[0] = flashrb(i, 128, 1)
+
+# Candy is dandy but liquor is quicker!
+  neopixelsd2[0] = wheeln(i, 0)
+  neopixelsd2[1] = wheeln(i, 32)
+  neopixelsd2[2] = wheeln(i, 64)
+  neopixelsd2.show()
+  neopixelsd4[0] = wheeln(i, 128)
+  neopixelsd4[1] = wheeln(i, 160)
+  neopixelsd4[2] = wheeln(i, 192)
+  neopixelsd4.show()
+  dot[0] = wheeld(i)
+
+
+# use D3 as capacitive touch to turn on internal LED
 #  if touch.value:
 #      print("D3 touched!")
 #  led.value = touch.value
